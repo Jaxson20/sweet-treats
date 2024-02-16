e092a21445989125260ac596c2d4ab88a35190cc
Jaxson20 <jaxsonfellhauerschool@gmail.com>
2024-02-16 11:13:17 -0600

    schema

---
+++ server/models/valentineOrder.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const ValentineOrderSchema = new Schema({
+  firstName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  phoneNumber: {
+    type: String,
+    required: false,
+    trim: true,
+  },
+  numberOfBoxes: {
+    type: String,
+    required: true,
+  },
+  specialMessage: {
+  type: String,
+  required: false,
+  trim: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const ValentineOrder = mongoose.model('Valentine Order', ValentineOrderSchema);
+
+module.exports = ValentineOrder;
